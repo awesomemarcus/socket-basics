d5683a56c52b30fc4658a9ac4bae34d96e00d584
Mark Jason Aquiatin <marcus@snapzio.com>
2016-05-05 11:16:11 +0800

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,13 @@ var room = getQueryVariable('room');
 
 socket.on('connect', function() {
   console.log('Connected to socket.io server!');
+  socket.emit('joinRoom', {
+    name: name,
+    room: room
+  });
 });
 
+jQuery('.room-title').text(room);
 
 socket.on('message', function(message) {
   var momentTimestamp = moment.utc(message.timestamp);
