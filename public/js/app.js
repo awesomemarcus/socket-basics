617b5e490395d042dbe77e3f7406a7501fdf5677
Mark Jason Aquiatin <marcus@snapzio.com>
2016-05-05 12:37:04 +0800

    Add some front end validation

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 
 socket.on('connect', function() {
-  console.log('Connected to socket.io server!');
+  // console.log('Connected to socket.io server!');
   socket.emit('joinRoom', {
     name: name,
     room: room
@@ -16,8 +16,8 @@ jQuery('.room-title').text(room);
 socket.on('message', function(message) {
   var momentTimestamp = moment.utc(message.timestamp);
   var $message = jQuery('.messages');
-  console.log('New message:');
-  console.log(message);
+  // console.log('New message:');
+  // console.log(message);
 
   $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local()
     .format(
